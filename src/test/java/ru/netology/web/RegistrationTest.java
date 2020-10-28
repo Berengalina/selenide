772f5b51deda60d48db37c69432faa71521b325c
Anna Belousova <annabelousovva@gmail.com>
2020-10-28 22:33:32 +0300

    Edit RegistrationTest.java

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -23,7 +23,19 @@ public class RegistrationTest {
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=notification]").waitUntil(text("Успешно!"), 15000).shouldBe(visible);
+    }
 
+    @Test
+    void shouldNotFillName() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Пермь");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE));
+        $("[data-test-id=date] input").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79266858100");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=name].input_invalid").shouldHave(exactText("Фамилия и имя Поле обязательно для заполнения"));
     }
 }
 
